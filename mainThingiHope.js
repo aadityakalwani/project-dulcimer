e3ec36b69509b069066e5e5835bed6f4bf03969d
Aadi <aadityakalwani@gmail.com>
2024-05-02 12:22:35 +0100

    using prompt-sync to take inputs, we're working now! committing everything

--- mainThingiHope.js
+++ mainThingiHope.js
@@ -1,3 +1,15 @@
 console.log("Hello World!");
-console.log("im creating this to do the first proper commit and see if everything is working fine");
-console.log("I can confirm it does");
+
+// so let's begin doing actual stuff
+// to take a user input:
+
+// prompt-sync is a module that allows you to take user input
+// this is much better and prettier than how javascript does it by itself 
+
+const prompt = require('prompt-sync')();
+
+const name = prompt('Enter your name: ');
+const age = prompt('Enter your age: ');
+
+console.log(`Hello ${name}!`);
+console.log(`You are ${age} years old!`);
