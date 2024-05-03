a19036a77788bfe03872e9384fb6d48f57ead578
Aadi <aadityakalwani@gmail.com>
2024-05-03 22:23:50 +0100

    beginning getValidInt

--- mainThingiHope.js
+++ mainThingiHope.js
@@ -1,5 +1,6 @@
 // prompt-sync is a module that allows us to take user input 
 // much better and prettier than how javascript does it by itself
+const readline = require("node:readline");
 const prompt = require('prompt-sync')();
 
 const name = prompt('Enter your name: ');
@@ -9,10 +10,10 @@ console.log(`Hello ${name}!`);
 console.log(`You are ${age} years old!`);
 
 var adjacencyMatrix;
-// set this as a 2d array later
+// take an input for dimensions of the matrix / number of nodes etc. and then set up a 2d array as required
 
 // menu system
-let userMenuOption = prompt("1 - apply Dijkstra\n2 - apply Prim's\n3 - apply Kruskal's:\n");
+let userMenuOption = getValidInt("1 - apply Dijkstra\n2 - apply Prim's\n3 - apply Kruskal's:\n", 1, 3)
 
 switch (userMenuOption) 
 {
@@ -47,4 +48,40 @@ function applyPrims(adjacencyMatrix)
 function applyKruskal(adjacencyMatrix)
 {
     console.log("doing Kruskal's");
+}
+
+/*
+c# code to getValidInt
+
+public int GetValidInt(int min, int max)
+        {
+            if (int.TryParse(Console.ReadLine(), out int validInt))
+            {
+                if (validInt >= min && validInt <= max)
+                {
+                    return validInt;
+                }
+
+                Console.WriteLine($"No bro enter a number between {min} and {max}");
+                return GetValidInt(min, max);
+            }
+
+            Console.WriteLine("Please enter a valid integer");
+            return GetValidInt(min, max);
+        }
+
+ */
+// translating to JS:
+
+function getValidInt(userPrompt, min, max) {
+    const attemptedInput = prompt(userPrompt);
+    if (Number.isInteger(attemptedInput)) {
+        if (attemptedInput >= min && attemptedInput <= max)
+        {
+            return attemptedInput;
+        }
+
+        console.log(`No bro enter a number between ${min} and ${max}`);
+        return getValidInt(min, max);
+    }
 }
